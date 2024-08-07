d938f827db2a681a296f6c40dc9970e8e58a793d
fenyan-16 <duanox@hotmail.com>
2024-08-07 23:25:16 +0200

    switch to proper default page

--- homepage/views.py
+++ homepage/views.py
@@ -3,9 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'homepage/index_d.html', {})
-
-def index2(request):
     return render(request, 'homepage/index.html', {})
 
 def bingo(request):
